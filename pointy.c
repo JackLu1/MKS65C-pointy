912e3eb55427c738c8b80972d87ae5ea81c37447
jlu <jlu2025@gmail.com>
2018-09-18 01:12:45 -0400

    fixed some stuff

--- pointy.c
+++ pointy.c
@@ -4,30 +4,36 @@ int main(){
 
     // int test = 12345; // 3039 in hex
     unsigned int a = 3847569123;
-    char *ip = &a;
+    char *pointy = (char*)&a;
 
-    printf("a: %x\n", a);
-    printf("a: %u\n", a);
+    printf("unsigned int a = %u\n", a);
+    printf("unsigned int a in hex = %x\n", a);
+    printf("pointy at address: %p\n\n", pointy);
 
-    printf("\n\nprint each byte\n" );
-    for (int i = 0; i < sizeof(a); i++){
-        printf("%x\n", *(ip + i));
+    printf("print each byte\n");
+    for (int i = 0; i < 4; i++){
+        printf("%hhx\n", *pointy);
+        printf("pointy at address: %p\n", pointy);
+        pointy++;
     }
 
-    printf("\n\nincrement by 1\n" );
-    for (int i = 0; i < sizeof(a); i++){
-        *(ip + i) += 1;
-        printf("hex: %x\n", *(ip + i));
-        printf("decimal: %d\n", *(ip + i));
+    pointy -= 4;
+    printf("\nincrement each byte by 1\n");
+    for (int i = 0; i < 4; i++){
+        (*pointy)++;
+        printf("%hhx\n", *pointy);
+        pointy++;
     }
+    printf("%u\n", a);
 
-    a = 3847569123;
-    printf("\n\nincrement by 16\n" );
-    for (int i = 0; i < sizeof(a); i++){
-        *(ip + i) += 16;
-        printf("hex: %x\n", *(ip + i));
-        printf("decimal: %d\n", *(ip + i));
+    pointy -= 4;
+    printf("\nincrement each byte by 16\n");
+    for (int i = 0; i < 4; i++){
+        (*pointy)+= 16;
+        printf("%hhx\n", *pointy);
+        pointy++;
     }
+    printf("%u\n", a);
 
     return 0;
 }
